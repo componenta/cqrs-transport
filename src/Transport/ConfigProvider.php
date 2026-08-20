15fdf419592457a13ba065b356d8bb051348bdb7
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2026-08-20 21:41:16 +0200

    Register safe default operation context serializer

--- src/Transport/ConfigProvider.php
+++ src/Transport/ConfigProvider.php
@@ -6,18 +6,25 @@ namespace Componenta\CQRS\Transport;
 
 use Componenta\Config\ConfigProvider as BaseConfigProvider;
 use Componenta\CQRS\Command\Middleware\TransportMiddleware;
+use Componenta\CQRS\Command\Transport\JsonOperationContextSerializer;
+use Componenta\CQRS\Command\Transport\OperationContextSerializerInterface;
 use Componenta\CQRS\ConfigKey;
 use Componenta\CQRS\Transport\Attribute\Async;
 
 final class ConfigProvider extends BaseConfigProvider
 {
-    /**
-     * @return array<string, list<class-string>>
-     */
+    /** @return array<string, list<class-string>> */
     protected function getConfig(): array
     {
         return [
             ConfigKey::COMMAND_METADATA_ATTRIBUTES => [Async::class],
+        ];
+    }
+
+    protected function getInvokables(): array
+    {
+        return [
+            OperationContextSerializerInterface::class => JsonOperationContextSerializer::class,
         ];
     }
 
